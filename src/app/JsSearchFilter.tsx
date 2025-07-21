b1c759dd32bfe8ca6a20894494bd283fc96bfaf7
pjs <bavuchoko@naver.com>
2025-07-21 09:26:43 +0900

    [add] 필터 옆 아이콘 늘어나면 드래그 되도록

--- src/app/JsSearchFilter.tsx
+++ src/app/JsSearchFilter.tsx
@@ -1,10 +1,11 @@
-import {FC, useState} from "react";
+import {FC, useRef, useState} from "react";
 import {Filter, FilterProps} from "./type/Types";
 import '../index.css';
 import {useFilterHandle} from "./hook/useToggleModal";
 import FilterIcon from "./uitls/FilterIcon";
 import Modal from "./uitls/Modal";
 import {useToggleModal} from "./hook/useFilterHandle";
+import useDragScroll from "./hook/useDragScroll";
 
 
 const JsSearchFilter: FC<FilterProps> =({filter =[], onValueChange=undefined, onSearch=undefined})=>{
@@ -13,6 +14,8 @@ const JsSearchFilter: FC<FilterProps> =({filter =[], onValueChange=undefined, on
 
     const { isOpen, open, close, toggle } = useToggleModal();
     const { values, handle, reset } = useFilterHandle(onValueChange);
+    const groupRef = useRef<HTMLDivElement>(null);
+    useDragScroll([groupRef]);
 
     return (
         <div style={{width:"100%"}}>
@@ -30,22 +33,36 @@ const JsSearchFilter: FC<FilterProps> =({filter =[], onValueChange=undefined, on
                     onClick={toggle}
                 />
 
-                {filter.map(el=>(
-                    <span
-                        style={{
-                            padding:'4px 8px',
-                            margin:'0 5px',
-                            cursor:'pointer',
-                            fontWeight:'bold',
-                            display:'flex',
-                            fontSize:'12px',
-                            alignItems:'center',
-                            color:  'var(--grayText)',
-                            border:'1px solid var(--border)'
-                        }}
-                        onClick={()=>{setClicked(el); toggle();}}
-                    >{el.label}</span>
-                ))}
+                <div
+                    className={'no-scroll no-drag'}
+                    style={{
+                        padding:"0 12px",
+                        display:'flex',
+                        alignItems:'center',
+                        width:'100%',
+                        overflowX:'hidden',
+                        whiteSpace: 'nowrap'
+                    }}
+                    ref={groupRef}
+                >
+                    {filter.map(el=>(
+                        <span
+                            data-id={el.key}
+                            style={{
+                                padding:'4px 8px',
+                                margin:'0 5px',
+                                cursor:'pointer',
+                                fontWeight:'bold',
+                                display:'flex',
+                                fontSize:'12px',
+                                alignItems:'center',
+                                color:  'var(--grayText)',
+                                border:'1px solid var(--border)'
+                            }}
+                            onClick={()=>{setClicked(el); toggle();}}
+                        >{el.label}</span>
+                    ))}
+                </div>
             </div>
 
             { isOpen && <Modal close={close} filter={filter} values={values}  handle={handle} reset={reset} clicked={clicked} setClicked={setClicked} onSearch={onSearch}/> }
